158bc0f9be076c77ded9363e3243a79cd13053af
Jay Whittingham <jmlwhittingham@gmail.com>
2022-06-21 14:30:13 +0100

    still working on button functions

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,12 +1,15 @@
 import Buttons from "../Buttons/buttons";
 import './App.css';
+import EachWeek from "../Buttons/eachWeek"
 
 function App() {
   return (
     <div className="App">
     <h1>Dashboard</h1>
 
-<Buttons></Buttons>
+<EachWeek></EachWeek>
+
+
 
     </div>
   );
--- front/src/components/Buttons/buttons.js
+++ front/src/components/Buttons/buttons.js
@@ -3,23 +3,24 @@ import "./buttons.css";
 
 export default function Buttons() {
 
-    return (<div>
+    return ( <div>
+
 <h1>Weekly quizzes!</h1> 
 
-<button onClick={() => alert("Please enter Mondays quiz results")} className="Monday" >See Mondays quiz!</button>
-<button onClick={() => alert("Please enter Tuesday quiz results")} className="Tuesday">See Tuesday quiz!</button>
-<button onClick={() => alert("Please enter Wednesday quiz results")} className="Wednesday">See Wednesday quiz!</button>
-<button onClick={() => alert("Please enter Thursday quiz results")} className="Thursday">See Thursday quiz!</button>
-<button onClick={() => alert("Please enter Friday quiz results")} className="Friday">See Friday quiz!</button>
+<button className="Monday" >See Mondays quiz!</button>
+<button className="Tuesday">See Tuesday quiz!</button>
+<button className="Wednesday">See Wednesday quiz!</button>
+<button className="Thursday">See Thursday quiz!</button>
+<button className="Friday">See Friday quiz!</button>
     
-{weeklyRecap()}
+{WeeklyRecap()}
     </div>)
 
 }   
-<br></br>
-function weeklyRecap() {
+
+function WeeklyRecap() {
     return (<div>
-        <br></br>
+        
         <h1>Weekend Recap</h1>
         <button className="week1"> Week 1 Recap!</button>
         <button className="week2"> Week 2 Recap!</button>
@@ -41,3 +42,25 @@ function weeklyRecap() {
     </div>)
 }
 
+// function EachWeek() {
+//     return (
+//         <div><button className="week1"> Week 1 </button>
+//         <button className="week2"> Week 2 </button>
+//         <button className="week3"> Week 3 </button>
+//         <button className="week4"> Week 4 </button>
+//         <button className="week5"> Week 5 </button>
+//         <button className="week6"> Week 6 </button>
+//         <button className="week7"> Week 7 </button>
+//         <button className="week8"> Week 8 </button>
+//         <button className="week9"> Week 9 </button>
+//         <button className="week10"> Week 10 </button>
+//         <button className="week11"> Week 11 </button>
+//         <button className="week12"> Week 12 </button>
+//         <button className="week13"> Week 13 </button>
+//         <button className="week14"> Week 14 </button>
+//         <button className="week15"> Week 15 </button>
+//         <button className="week16"> Week 16 </button>
+//         </div>
+//     )
+    
+// }
