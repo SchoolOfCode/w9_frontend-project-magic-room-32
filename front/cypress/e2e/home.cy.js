d86d6ce33ee491677dc76e48d77bd2e19c5c35b5
Lucy de Rojas <lucyderojas@gmail.com>
2022-06-29 16:03:23 +0100

    ff

--- front/cypress/e2e/home.cy.js
+++ front/cypress/e2e/home.cy.js
@@ -1,3 +1,4 @@
+const { wait } = require("@testing-library/user-event/dist/utils");
 
 console.clear();
 
@@ -24,39 +25,93 @@ describe('home page tests', () => {
     cy.get('#EachWeek > #3').click();
     cy.get('#chart > h1').contains('Quiz results for Week 3');
     
-   
-
-    
+      
   });
 
 
 
+  it('click 7, click 15, check header 1 content', ()=> {
 
-  it('click 1, click 2 and check header 1 content', ()=> {
-
-        
         cy.visit('http://localhost:3000/');
+        cy.get('#EachWeek > #7').click();
+        cy.get('#chart > h1').contains('Quiz results for Week 7');
         
-        cy.get('#EachWeek > #3').should('exist');
+        
+        cy.get('#EachWeek > #15').click();
+        cy.get('#chart > h1').contains('Quiz results for Week 15');
+
+});
+
+
+
+// ✅ fill in diary
+        it('fill in diary and press submit', ()=> {
+                cy.visit('http://localhost:3000/');
+                cy.get('#input-diary').type('hello');
+
+                cy.get('form > button').should('exist');
+                cy.get('form > button').click();
+                wait(3000);
+
+        });
+
+
+
+// ✅ radio check wednesday
+        it('radio:> check wednesday, ',()=> {
+                cy.visit('http://localhost:3000/');
+                cy.get('#EachWeek > #13').click();
+
+                cy.get('#quizzNumberSelect > #3 ').click();
+
+                cy.wait(3000)
+
+                
+        });
+
+
+
+
+        // 🏀  sliding slider:
+        it('slide slider', ()=> {
+
+                cy.viewport(700, 900)
+
+                cy.visit('http://localhost:3000/');         
+                cy.get('#EachWeek > #1').click();
+
+
+                // cy.get('#slider').should('exist');
+                
+                cy.wait(3000);
+                cy.get('#slider').invoke('val', 8).trigger('change').click({force: true});
+                cy.wait(3000);
+                cy.get('#quizzNumberSelect > #3 ').click();
+                cy.wait(3000);
+                cy.get('#input-diary').type('hello');
+
+
+
+
+                cy.get('#quizzInput > :nth-child(3)').contains('10');
+
+                
+
+
+
+                
+
+        });
+
+
+
 
         
-        
-       
-        
-  
-  }); 
 
 
 
 
 
-
-//   it('press button 4', ()=> {
-//         cy.visit(homePage);
-//         cy.get('#EachWeek > #4').click();
-//         cy.get('#chart > h1').contains('Quiz results for Week 4 ');
-
-//   });
 
 
 
@@ -71,3 +126,6 @@ describe('home page tests', () => {
 
 
 })  // end of describe
+
+
+
